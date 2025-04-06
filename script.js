a38ff64b194ba7f57b1b32d89aa50100c7782a58
Inji Mammadli <mammi910@newschool.edu>
2025-04-06 19:33:11 -0400

    further updates on style - mobile - gift display

--- script.js
+++ script.js
@@ -53,33 +53,48 @@ const displayGifts = (npcSelect) => {
 
     const npcDisplay = document.getElementById('npc-display')
 
-    let item = `<button onclick="goBack()">Back to NPC List</button><h2>${selectedNPC.name}</h2><img src="${selectedNPC.image}"></img><p>${selectedNPC.Birthday}</p>
-    <section><h3>Loved gifts</h3><ul>`
+    let item = `<button class="link-button" onclick="goBack()">NPC List</button>
+    <section class="npc-info">
+        <section class="npc-frame">
+            <img src="${selectedNPC.image}"></img>
+            <h2>${selectedNPC.name}</h2>
+        </section>
+         <p class="birthday"><img src="assets/cake.png" alt="Birthday" /> ${selectedNPC.Birthday}</p>
+        <div class="gift-nav">
+            <button class="tags" onclick="scrollToCategory('loved')">Loved</button>
+            <button class="tags" onclick="scrollToCategory('liked')">Liked</button>
+            <button class="tags" onclick="scrollToCategory('neutral')">Neutral</button>
+            <button class="tags" onclick="scrollToCategory('disliked')">Disliked</button>
+            <button class="tags" onclick="scrollToCategory('hated')">Hated</button>
+        </div>
+    </section>
+    
+    <section class="gift-grid"><section id="loved"><h3>Loved gifts</h3><ul>`
 
     selectedNPC.Loves.forEach(lovedGift => {
         item = item + `<li><span>${lovedGift.name}</span><img src="${lovedGift.image}"></img></li>`
     })
-    item = item + '</ul></section><section class="gift-section-container"><h3>Liked Gifts</h3><ul>'
+    item = item + '</ul></section><section id="liked"><h3>Liked Gifts</h3><ul>'
 
     selectedNPC.Likes.forEach(likedGift => {
         item = item + `<li><span>${likedGift.name}</span><img src="${likedGift.image}"></img></li>`
     })
-    item = item + '</ul></section><h3>Neutral Gifts</h3><ul>'
+    item = item + '</ul></section><section id="neutral"><h3>Neutral Gifts</h3><ul>'
 
     selectedNPC.Neutral.forEach(neutralGift => {
         item = item + `<li><span>${neutralGift.name}</span><img src="${neutralGift.image}"></img></li>`
     })
-    item = item + '</ul></section><h3>Disliked Gifts</h3><ul>'
+    item = item + '</ul></section><section id="disliked"><h3>Disliked Gifts</h3><ul>'
 
     selectedNPC.Dislikes.forEach(dislikedGift => {
         item = item + `<li><span>${dislikedGift.name}</span><img src="${dislikedGift.image}"></img></li>`
     })
-    item = item + '</ul></section><h3>Hated Gifts</h3><ul>'
+    item = item + '</ul></section><section id="hated"><h3>Hated Gifts</h3><ul>'
 
     selectedNPC.Hates.forEach(hatedGift => {
         item = item + `<li><span>${hatedGift.name}</span><img src="${hatedGift.image}"></img></li>`
     })
-    item = item + '</ul></section>'
+    item = item + '</ul></section></section>'
 
 
     npcDisplay.innerHTML = item;
@@ -130,6 +145,9 @@ function showUniversalGifts(category) {
     modal.classList.remove("hidden");
 }
 
+
+// interaction functions
+
 function closeModal() {
     document.getElementById("universalModal").classList.add("hidden");
 }
@@ -138,8 +156,15 @@ function closeModal() {
 function goBack() {
     document.getElementById('npc-display').classList.add('hidden');
     document.getElementById('npc-list').classList.remove('hidden');
-  }
+}
 
+
+function scrollToCategory(id) {
+    const el = document.getElementById(id);
+    if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+    }
+}
 
 
 
