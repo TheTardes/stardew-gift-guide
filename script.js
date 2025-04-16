0eb8b1c917abdebd872bcf79441763d0ce8efb6d
Inji Mammadli <mammi910@newschool.edu>
2025-04-15 23:41:30 -0400

    added - top button; removed search

--- script.js
+++ script.js
@@ -100,21 +100,21 @@ const displayGifts = (npcSelect) => {
 }
 
 
-const npcSearch = document.getElementById('npc-search');
-const npcSelect = document.getElementById('npc-select');
+// const npcSearch = document.getElementById('npc-search');
+// const npcSelect = document.getElementById('npc-select');
 
-// getting input in the search bar
-npcSearch.addEventListener('input', () => {
-    const inputValue = npcSearch.value.toLowerCase().trim();
+// // getting input in the search bar
+// npcSearch.addEventListener('input', () => {
+//     const inputValue = npcSearch.value.toLowerCase().trim();
 
-    // matching NPC by name
-    const match = list.find(npc => npc.name.toLowerCase().startsWith(inputValue));
+//     // matching NPC by name
+//     const match = list.find(npc => npc.name.toLowerCase().startsWith(inputValue));
 
-    if (match) {
-        npcSelect.value = match.name;
-        displayGifts(npcSelect);
-    }
-});
+//     if (match) {
+//         npcSelect.value = match.name;
+//         displayGifts(npcSelect);
+//     }
+// });
 
 
 
@@ -166,12 +166,33 @@ function scrollToCategory(id) {
 
 function openModal(id) {
     document.getElementById(id).classList.remove('hidden');
-  }
-  
-  function closeModal(id) {
+}
+
+function closeModal(id) {
     document.getElementById(id).classList.add('hidden');
+}
+
+
+
+// Get the button:from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+let topButton = document.getElementById("scroll-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
   }
-  
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
